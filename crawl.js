87cd4e0451d77105c7eb680054a4808339adc4e2
jespieee <mhuhu@umich.edu>
2024-08-11 20:10:06 -0500

    add basic crawler function and output raw HTML

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+import { JSDOM } from 'jsdom'
+
 function normalizeURL(url) {
     const urlObj = new URL(url)
     let fullPath = `${urlObj.host}${urlObj.pathname}`
@@ -7,4 +9,46 @@ function normalizeURL(url) {
     return fullPath
 }
 
-export { normalizeURL };
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const anchors = dom.window.document.querySelectorAll('a')
+
+    for (const anchor of anchors) {
+        if (anchor.hasAttribute('href')) {
+            let href = anchor.getAttribute('href')
+            try {
+                href = new URL(href, baseURL).href
+                urls.push(href)
+            } catch(err) {
+                console.log(`${err.message}: ${href}`)
+            }
+        }
+    }
+    return urls
+}
+
+async function crawlPage(url) {
+
+    let response
+    try {
+        response = await fetch(url)
+    } catch(err) {
+        throw new Error(`Got Network error: ${err.message}`)
+    }
+
+    if (response.status > 399) {
+        console.log(`Got HTTP error" ${response.status} ${response.statusText}`)
+        return
+    }
+
+    const contentType = response.headers.get('content-type')
+    if (!contentType || !contentType.includes('text/html')) {
+        console.log(`Content-type is: ${contentType}`)
+        return
+    }
+
+    console.log(await response.text())
+}
+
+export { normalizeURL, getURLsFromHTML, crawlPage};
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+import { crawlPage } from "./crawl.js"
+
+async function main() {
+    if (process.argv.length < 3) {
+        console.log('The number of arguments was less than 1, exiting!')
+        return
+    }
+    else if (process.argv.length > 3) {
+        console.log('The number of arguments was greater than 1, exiting!')
+        return
+    }
+    const baseURL = process.argv[2]
+    
+    console.log(`Beginning to crawl starting at: ${baseURL}`)
+        
+    await crawlPage(baseURL)
+}
+
+main()
